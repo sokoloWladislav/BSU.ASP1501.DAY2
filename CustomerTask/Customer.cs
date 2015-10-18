34c9bd4e9df11677677cf9f054a4a1676c4d7f54
Uladzislau_Sakalou <needyoursoul2die@gmail.com>
2015-10-18 21:17:43 +0300

    added Customer task

---
+++ CustomerTask/Customer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CustomerTask
+{
+    public delegate string StringFormat();
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string ContactPhone { get; set; }
+        public decimal Revenue { get; set; }
+        private static List<StringFormat> stringFormates = new List<StringFormat>();
+        public Customer(string name, string contactPhone, decimal revenue)
+        {
+            Name = name;
+            ContactPhone = contactPhone;
+            Revenue = revenue;
+            stringFormates.Add(StringFormat1);
+            stringFormates.Add(StringFormat2);
+            stringFormates.Add(StringFormat3);
+            stringFormates.Add(StringFormat4);
+            stringFormates.Add(StringFormat5);
+        }
+
+        public string Show(int index)
+        {
+            return stringFormates[index]();
+        }
+
+        public static void AddNewStringFormat(StringFormat s)
+        {
+            stringFormates.Add(s);
+        }
+
+        private string StringFormat1()
+        {
+            NumberFormatInfo format = (NumberFormatInfo)CultureInfo.CurrentCulture.NumberFormat.Clone();
+            format.CurrencyDecimalSeparator = ".";
+            format.CurrencyDecimalDigits = 2;
+            format.CurrencySymbol = "";
+           
+            return String.Format(format, "Customer record: {0}, {1:C}, {2}", Name, Revenue, ContactPhone);
+        }
+
+        private string StringFormat2()
+        {
+            return String.Format("Customer record: {0}", ContactPhone);
+        }
+
+        private string StringFormat3()
+        {
+            NumberFormatInfo format = (NumberFormatInfo)CultureInfo.CurrentCulture.NumberFormat.Clone();
+            format.CurrencyDecimalSeparator = ".";
+            format.CurrencyDecimalDigits = 2;
+            format.CurrencySymbol = "";
+            return String.Format(format, "Customer record: {0}, {1:C}", Name, Revenue);
+        }
+
+        private string StringFormat4()
+        {
+            return String.Format("Customer record: {0}", Name);
+        }
+
+        private string StringFormat5()
+        {
+            return String.Format("Customer record: {0}", Revenue);
+        }
+    }
+}
---
+++ TestCustomer/Test_Customer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomerTask;
+using System.Globalization;
+
+namespace TestCustomer
+{
+    [TestClass]
+    public class Test_Customer
+    {
+        [TestMethod]
+        public void Test_Show_Format1()
+        {
+            string Name = "Jeffrey Richter";
+            string ContactPhone = "+1 (425) 555-0100";
+            decimal Revenue = 1000000;
+            NumberFormatInfo format = (NumberFormatInfo)CultureInfo.CurrentCulture.NumberFormat.Clone();
+            format.CurrencyDecimalSeparator = ".";
+            format.CurrencyDecimalDigits = 2;
+            format.CurrencySymbol = "";
+            string expected = String.Format(format, "Customer record: {0}, {1:C}, {2}", Name, Revenue, ContactPhone);
+
+            Customer customer = new Customer(Name, ContactPhone, Revenue);
+            string actual = customer.Show(0); 
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Show_Format2()
+        {
+            string Name = "Jeffrey Richter";
+            string ContactPhone = "+1 (425) 555-0100";
+            decimal Revenue = 1000000;
+            string expected = "Customer record: +1 (425) 555-0100";
+
+            Customer customer = new Customer(Name, ContactPhone, Revenue);
+            string actual = customer.Show(1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Show_Format3()
+        {
+            string Name = "Jeffrey Richter";
+            string ContactPhone = "+1 (425) 555-0100";
+            decimal Revenue = 1000000;
+            NumberFormatInfo format = (NumberFormatInfo)CultureInfo.CurrentCulture.NumberFormat.Clone();
+            format.CurrencyDecimalSeparator = ".";
+            format.CurrencyDecimalDigits = 2;
+            format.CurrencySymbol = "";
+            string expected = String.Format(format, "Customer record: {0}, {1:C}", Name, Revenue);
+
+            Customer customer = new Customer(Name, ContactPhone, Revenue);
+            string actual = customer.Show(2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Show_Format4()
+        {
+            string Name = "Jeffrey Richter";
+            string ContactPhone = "+1 (425) 555-0100";
+            decimal Revenue = 1000000;
+            string expected = "Customer record: Jeffrey Richter";
+
+            Customer customer = new Customer(Name, ContactPhone, Revenue);
+            string actual = customer.Show(3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Show_Format5()
+        {
+            string Name = "Jeffrey Richter";
+            string ContactPhone = "+1 (425) 555-0100";
+            decimal Revenue = 1000000;
+            string expected = "Customer record: 1000000";
+
+            Customer customer = new Customer(Name, ContactPhone, Revenue);
+            string actual = customer.Show(4);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
