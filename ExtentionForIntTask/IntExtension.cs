eea10797b272357b0dea40ba9c431c4de8d9cfd0
Uladzislau_Sakalou <needyoursoul2die@gmail.com>
2015-10-19 01:33:52 +0300

    added Int Extension task

---
+++ ExtentionForIntTask/IntExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionForIntTask
+{
+    public static class IntExtension
+    {
+        public static string StringFormat16(this int number)
+        {
+            string result = "0";
+            if (number != 0)
+            {
+                bool isNegative = (number < 0) ? true: false;
+                char[] digits = "0123456789ABCDEF".ToCharArray();
+                StringBuilder sb = new StringBuilder();
+                if (isNegative)
+                    number *= -1;
+                while (number != 0)
+                {
+                    sb.Insert(0, digits[number % 16]);
+                    number /= 16;
+                }
+                if (isNegative)
+                    sb.Insert(0, "-");
+                result = sb.ToString();
+            }
+            return result;
+        }
+    }
+}
---
+++ TestExtensionForInt/Test_IntExtension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtentionForIntTask;
+
+namespace TestExtensionForInt
+{
+    [TestClass]
+    public class Test_IntExtension
+    {
+        [TestMethod]
+        public void Test_StringFormat16_16ToStringReturned10()
+        {
+            int number = 16;
+            string expected = "10";
+
+            string actual = number.StringFormat16();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_StringFormat16_458ToStringReturned1CA()
+        {
+            int number = 458;
+            string expected = "1CA";
+
+            string actual = number.StringFormat16();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_StringFormat16_0ToStringReturned0()
+        {
+            int number = 0;
+            string expected = "0";
+
+            string actual = number.StringFormat16();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_StringFormat16_Negative745ToStringReturned2E9()
+        {
+            int number = -745;
+            string expected = "-2E9";
+
+            string actual = number.StringFormat16();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
